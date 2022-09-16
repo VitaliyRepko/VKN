59131f23e9c5f1b7846626130f953da09a8d89e7
VitaliyRepko <113344054+VitaliyRepko@users.noreply.github.com>
2022-09-16 13:13:24 +0300

    Delete 4.2 завдання.py

--- 4.2 завдання.py
+++
@@ -1,8 +0,0 @@
-import math
-x=int(input("Введіть значення x: "))
-y=int(input("Введіть значення y: "))
-z=int(input("Введіть значення z: "))
-a=int(input("Введіть значення a: "))
-L=(x*math.cos(y)+z)-((math.tan(y)+math.pow(math.e, a))/math.pow(2, z+2.4))
-print("Значення виразу дорівнює", L)
-input()
