744b351cec276d402e65365d8dedaa2d7edee584
VitaliyRepko <113344054+VitaliyRepko@users.noreply.github.com>
2022-09-16 13:13:19 +0300

    Delete 4.1 завдання.py

--- 4.1 завдання.py
+++
@@ -1,5 +0,0 @@
-import math
-x=int(input("Введіть значення x: "))
-f=float((pow(math.e, x+math.sqrt(x)+math.cos(x)))*(2.9*x-(math.cos(x)/math.sin(x)))/(pow(3, 0.7*x+math.sqrt(x))))
-print("Значення функції дорівнює", f)
-input()
